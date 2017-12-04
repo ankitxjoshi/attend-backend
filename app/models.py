87a6fb019c417656159942de84f46630543ecfbc
Shivam <sharmashivam3050@gmail.com>
2017-12-04 12:50:49 +0530

    Routes Defined for Student

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from app import db
 from flask import current_app as app
 
 __author__ = 'Ankit Joshi'
-
+print()
 
 class Admin(db.Model):
     __tablename__ = 'admins'
@@ -50,9 +50,76 @@ class Student(db.Model):
     email = db.Column(db.String(128))
     year = db.Column(db.Integer)
     phoneno = db.Column(db.String(10))
-    section = db.Column(db.Integer)
-    branch = db.Column(db.Integer)
+    section = db.Column(db.String(20))
+    branch = db.Column(db.String(20))
     image_url = db.Column(db.Text)
 
     def __repr__(self):
         return '<Student: {}>'.format(self.name)
+
+class Attendance(db.Model):
+    __tablename__ = 'attendance'
+
+    def _get_date():
+        return datetime.datetime.now()
+
+    id = db.Column(db.Integer, primary_key=True)
+    rollno = db.Column(db.String(7), index=True)
+    subject = db.Column(db.String(50))
+    period_id = db.Column(db.Integer,db.ForeignKey('period.id'))
+    date = db.Column(db.Date,default=_get_date)
+    presence_flag=db.Column(db.Boolean)
+
+
+
+    def __repr__(self):
+        return '<Attendance: {}>'.format(self.name)
+
+
+class TimeTable(db.Model):
+    __tablename__ = 'timetable'
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(50))
+    period_id = db.Column(db.Integer,db.ForeignKey('period.id'))
+    day = db.Column(db.String(10))
+    section = db.Column(db.String(20))
+    location = db.Column(db.String(20),db.ForeignKey('classroom.location'))
+    year = db.Column(db.Integer)
+    staff_id =db.Column(db.Integer,db.ForeignKey('staff.id'))
+
+    def __repr__(self):
+        return '<TimeTable: {}>'.format(self.name)
+
+
+class Classroom(db.Model):
+    __tablename__ = 'classroom'
+
+    location = db.Column(db.String(20), primary_key=True)
+    bluetooth_address = db.Column(db.String(50),unique=True)
+
+
+
+    def __repr__(self):
+        return '<Classroom: {}>'.format(self.name)
+
+
+class Period(db.Model):
+    __tablename__ = 'period'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.Time)
+    end_time = db.Column(db.Time)
+
+    def __repr__(self):
+        return '<Period: {}>'.format(self.name)
+
+
+class Staff(db.Model):
+    __tablename__ = 'staff'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Staff: {}>'.format(self.name)
---
+++ app/student/views.py
@@ -0,0 +1,179 @@
+from flask import abort, request, jsonify, g, url_for
+import os
+import json
+import datetime
+from . import  student
+from .. import db
+from ..models import Admin, Student ,TimeTable , Period , Classroom , Staff,Attendance
+from ..decorators import auth
+import constants as const
+from sqlalchemy import *
+
+__author__ = 'Shivam Sharma'
+
+# write the routes below
+@student.route('/classesOfDay/<string:rollno>',methods=['GET'])
+def get_classes_of_day(rollno):
+
+    # get the section and year
+    student_details = Student.query.filter_by(rollno=rollno).first()
+    if not student:
+        return jsonify(
+            status = const.status['BAD_REQUEST'],
+            message = const.string['USER_DOESNT_EXISTS']
+        )
+    print  "Student Details ", student_details.section,student_details.year
+    section , year = student_details.section,student_details.year
+
+
+    now = datetime.datetime.now()
+    day = now.strftime("%A")
+    # comment out the beow line
+    day = 'Monday'
+
+
+    time_table_details =  (db.session.query(\
+                            TimeTable.subject.label("subject"),\
+                            TimeTable.location.label("classroom"),\
+                            Staff.name.label("faculty_name"),\
+                            Period.start_time.label("begin_time"),\
+                            Period.end_time.label("end_time"),\
+                            Classroom.bluetooth_address.label("bluetooth_address"))\
+                            .join(Period)\
+                            .join(Staff)\
+                            .join(Classroom)\
+                            .filter(and_(TimeTable.day== day,TimeTable.section==section,TimeTable.year==year))\
+                            ).all()
+
+
+    timetable = [dict((name, getattr(x, name)) for name in ['subject', 'classroom','faculty_name',\
+                'begin_time','end_time','bluetooth_address']) for x in time_table_details]
+
+    print "time table ---------",timetable[1]
+
+
+    result = {}
+    result['status'] = const.status['OK']
+    result['message'] = const.string['SUCCESS']
+    result['data'] = timetable
+    return  json.dumps(result,indent=4, default=str)
+
+
+
+@student.route('/details/<string:rollno>',methods=['GET'])
+def get_student_details(rollno):
+    student = Student.query.filter_by(rollno=rollno).first()
+    if not student:
+        return jsonify(
+            status=const.status['BAD_REQUEST'],
+            message=const.string['USER_DOESNT_EXISTS']
+        )
+
+    print "Student details",dir(student)
+    data = dict((name, getattr(student, name)) for name in [ 'branch', 'email', 'id',\
+                'name', 'phoneno','rollno', 'section', 'year'])
+    print 'data--------------',data
+    return jsonify(
+        data=data,
+        image_path=student.image_url,
+        status=const.status['OK'],
+        message=const.string['SUCCESS']
+    )
+
+
+@student.route('/attendance_details_for_subject/<string:rollno>/<string:subject>',methods=['GET'])
+def get_attendance_details_for_subject(rollno,subject):
+
+    attendance_details = (db.session.query(\
+                          Attendance.date.label("date"),\
+                          Attendance.presence_flag.label("presence_flag"),\
+                          Attendance.period_id.label("period"),\
+                          Period.start_time.label("begin_time"),\
+                          Period.end_time.label("end_time"))\
+                          .join(Period)\
+                          .filter(and_(Attendance.subject == subject,Attendance.rollno == rollno))\
+                          ).all()
+
+
+    if not attendance_details:
+        return jsonify(
+            status=const.status['BAD_REQUEST'],
+            message=const.string['USER_DOESNT_EXISTS']
+        )
+
+
+    attendance = [dict((name, getattr(x, name)) for name in ['date', 'period',\
+                 'presence_flag','begin_time','end_time']) for x in attendance_details]
+
+
+    print "time table ---------",attendance[1]
+
+
+    result = {}
+    result['status'] = const.status['OK']
+    result['message'] = const.string['SUCCESS']
+    result['data'] = attendance
+    return  json.dumps(result,indent=4, default=str)
+
+
+
+
+@student.route('/attendance_summary_for_subject/<string:rollno>/<string:subject>',methods=['GET'])
+def get_attendance_summary_for_subject(rollno,subject):
+
+    total_present = (db.session.query(func.count(Attendance.id))\
+                               .filter(and_(Attendance.presence_flag == True,\
+                                            Attendance.subject == subject ,\
+                                            Attendance.rollno == rollno))).first()[0]
+
+    total_attendance = (db.session.query(func.count(Attendance.id))\
+                                  .filter(and_(Attendance.subject == subject ,\
+                                               Attendance.rollno == rollno))).first()[0]
+
+    data = {}
+    data['total_present'] = total_present
+    data['total_attendance'] = total_attendance
+
+    result = {}
+    result['status'] = const.status['OK']
+    result['message'] = const.string['SUCCESS']
+    result['data'] = data
+    return  json.dumps(result,indent=4, default=str)
+
+
+
+@student.route('/cummulative_attendance_summary/<string:rollno>',methods=['GET'])
+def get_cummulative_attendance_summary(rollno):
+    total_attendance = db.session.query(func.count(Attendance.id).label('count'),Attendance.subject)\
+                                 .filter(Attendance.rollno == rollno)\
+                                 .group_by(Attendance.subject).all()
+
+    total_present = db.session.query(func.count(Attendance.id).label('count'),Attendance.subject)\
+                              .filter(and_(Attendance.rollno == rollno ,\
+                                           Attendance.presence_flag==True))\
+                              .group_by(Attendance.subject).all()
+
+    total_subjects = [x.subject for x in total_attendance]
+    actual_subjects = [x.subject for x in total_present]
+    missing_subjects = list((set(total_subjects)-set(actual_subjects)))
+
+    [total_present.append((0l,x)) for x in missing_subjects]
+
+    total_present.sort(key = lambda x: x[1])
+    total_attendance.sort(key = lambda x: x[1])
+
+    cummulative_attendance_summary = []
+
+    for x,y in  zip(total_present,total_attendance):
+        temp={}
+        temp[x[1]]={ }
+        temp[x[1]]["total_attendance"] = x[0]
+        temp[x[1]]["total_present"] = y[0]
+        cummulative_attendance_summary.append(temp)
+
+    result = {}
+
+    result['status'] = const.status['OK']
+    result['message'] = const.string['SUCCESS']
+    result['data'] = cummulative_attendance_summary
+    return  json.dumps(result,indent=4, default=str)
